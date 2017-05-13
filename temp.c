6cd0eca5fd3b0a8134b90dccd22d82fba22e7253
ming <mjb940806@naver.com>
2017-05-14 02:41:50 +0900

    fix #2, FCFS

--- temp.c
+++ temp.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define CPUtime 50
+#define SCHEDULE 0
+#define IDLE -1
 
 typedef struct process PROCESS;
 typedef struct process {
@@ -12,44 +15,53 @@ typedef struct process {
 	int Priority;
 	int RemainTime;
 	int WaitingTime;
+	int ExecutingTime; /* to compute Turnaround time */
 	int Turnaround;
 	PROCESS *Next;
 } PROCESS;
 
 PROCESS *head = NULL, *tail = NULL;
-int processNum;
+PROCESS *readyHead = NULL, *readyTail = NULL;
+PROCESS *endHead = NULL, *endTail = NULL;
+int processNum, CPUstate;
 
 /*--- function declaration ---*/
-void Menu();
-void Select(int select);
 void CreateProcess();
 void Enqueue(PROCESS *node, PROCESS **head, PROCESS **tail);
-void PrintProcess(PROCESS **node);
-void SWAP(PROCESS *node1, PROCESS *node2);
+
+void Menu();
+void Select(int select);
+
+void PrintProcess(PROCESS **head);
+void PrintEvaluation();
+
 void SortByArrival(PROCESS **head, PROCESS **tail);
 void SortByBurst(PROCESS **head, PROCESS **tail);
-void SortByPriority(PROCESS **head, PROCESS **tail)
+void SortByPriority(PROCESS **head, PROCESS **tail);
+void SortByRemain(PROCESS **head, PROCESS **tail);
 
+void CopyNode(PROCESS *node, PROCESS *new);
+void CopyQueue(PROCESS **queue, PROCESS **head, PROCESS **tail);
 
 /*--- function definition ---*/
 void Menu() {
 
 	int select;
-	printf("      +----------------------------------------------------------------+\n");
-	printf("      |                    CPU Scheduling Simulator                    |\n");
-	printf("      +----------------------------------------------------------------+\n");
-	printf("      |                                                                |\n");
-	printf("      |   1. FCFS(First Come First Served)                             |\n");
-	printf("      |   2. Nonpreemptive SJF(Shortest Job First)                     |\n");
-	printf("      |   3. Preemptive SJF(Shortest Job First)                        |\n");
-	printf("      |   4. Nonpreemptive Priority                                    |\n");
-	printf("      |   5. Preemptive Priority                                       |\n");	
-	printf("      |   6. RR(Round Robin)                                           |\n");
-	printf("      |   7. Evaluation                                                |\n");
-	printf("      |   8. Exit                                                      |\n");
-	printf("      |                                                                |\n");
-	printf("      +----------------------------------------------------------------+\n\n");	
-	printf("          Select CPU Scheduling Algorithm : ");
+	printf("+----------------------------------------------------------------+\n");
+	printf("|                    CPU Scheduling Simulator                    |\n");
+	printf("+----------------------------------------------------------------+\n");
+	printf("|                                                                |\n");
+	printf("|   1. FCFS(First Come First Served)                             |\n");
+	printf("|   2. Nonpreemptive SJF(Shortest Job First)                     |\n");
+	printf("|   3. Preemptive SJF(Shortest Job First)                        |\n");
+	printf("|   4. Nonpreemptive Priority                                    |\n");
+	printf("|   5. Preemptive Priority                                       |\n");	
+	printf("|   6. RR(Round Robin)                                           |\n");
+	printf("|   7. Evaluation                                                |\n");
+	printf("|   8. Exit                                                      |\n");
+	printf("|                                                                |\n");
+	printf("+----------------------------------------------------------------+\n\n");	
+	printf("                 Select CPU Scheduling Algorithm : ");
 	scanf("%d", &select);
 	Select(select);
 
@@ -98,7 +110,10 @@ void CreateProcess() {
 
 	srand(time(NULL));
 
-	printf("Enter the number of process : ");
+	printf("+----------------------------------------------------------------+\n");
+	printf("|                         ProcessCreation                        |\n");
+	printf("+----------------------------------------------------------------+\n");
+	printf("                 Enter the number of process : ");
 	scanf("%d", &processNum);
 
 	for(i = 0;i < processNum; i++) {
@@ -110,6 +125,7 @@ void CreateProcess() {
 		node->Priority = rand() % processNum + 1;
 		node->RemainTime = node->CPUburstTime;
 		node->WaitingTime = 0;
+		node->ExecutingTime = 0;
 		node->Turnaround = 0;
 		node->Next = NULL;
 		Enqueue(node, &head, &tail);
@@ -126,32 +142,322 @@ void Enqueue(PROCESS *node, PROCESS **head, PROCESS **tail) {
 
 }
 
-void PrintProcess(PROCESS **queue) {
+void PrintProcess(PROCESS **head) {
 
 	printf("+-----+-------+--------+--------+-------+------+------+----------+\n");
 	printf("| PID |Arrival|Priority|CPUburst|IOburst|Remain| Wait |Turnaround|\n");
 	printf("+-----+-------+--------+--------+-------+------+------+----------+\n");	
 
-	for(; *queue != NULL; *queue = (*queue)->Next) {
-		printf("| %d |  %d  |  %d   |  %d   |  %d  | %d  | %d |   %d    |\n", 
-			(*queue)->ProcessID, (*queue)->ArrivalTime, (*queue)->Priority, (*queue)->CPUburstTime,
-			(*queue)->IOburstTime, (*queue)->RemainTime, (*queue)->WaitingTime, (*queue)->Turnaround);
+	for(; *head != NULL; *head = (*head)->Next) {
+		printf("| %3d |  %3d  |  %3d   |  %3d   |  %3d  | %3d  | %4d |   %3d    |\n", 
+			(*head)->ProcessID, (*head)->ArrivalTime, (*head)->Priority, (*head)->CPUburstTime,
+			(*head)->IOburstTime, (*head)->RemainTime, (*head)->WaitingTime, (*head)->Turnaround);
 		printf("+-----+-------+--------+--------+-------+------+------+----------+\n");
 	}
 
 }
 
-void SWAP(PROCESS *node1, PROCESS *node2) {
+void SortByArrival(PROCESS **head, PROCESS **tail) {
 
-	PROCESS swap;
+	PROCESS *FRONT, *MID, *END; /* for swap */
+	PROCESS *shead, *stail, *temp; 
+	int i;
 
-	swap = *node1;
-	*node1 = *node2;
-	*node2 = swap;
-	
+	/* shead -> head -> ... -> tail -> stail */
+	shead = (PROCESS *)malloc(sizeof(PROCESS));
+	stail = (PROCESS *)malloc(sizeof(PROCESS));
+	(*tail)->Next = (PROCESS *)malloc(sizeof(PROCESS));
+
+	shead->Next = *head;
+	stail = (*tail)->Next;
+	stail->Next = NULL;
+
+	/* bubble sort */
+	for (i=0; i<processNum; i++)
+	{
+		FRONT = shead;
+		MID = FRONT->Next;
+		END = MID->Next;
+
+		while(END!=stail) {
+
+			if(MID->ArrivalTime > END->ArrivalTime) {
+				FRONT->Next = END;
+				MID->Next = END->Next;
+				END->Next = MID;
+			}
+			FRONT = FRONT->Next;
+			MID = FRONT->Next;
+			END = MID->Next;
+		}
+	}
+
+	/* set head and tail */
+	*head = shead->Next;
+	MID->Next = NULL; /* to cut off stail */
+	*tail = FRONT->Next;
+
+	free(shead);
+	free(stail);
+
+}
+
+void SortByBurst(PROCESS **head, PROCESS **tail) {
+
+	PROCESS *FRONT, *MID, *END; /* for swap */
+	PROCESS *shead, *stail, *temp; 
+	int i;
+
+	/* shead -> head -> ... -> tail -> stail */
+	shead = (PROCESS *)malloc(sizeof(PROCESS));
+	stail = (PROCESS *)malloc(sizeof(PROCESS));
+	(*tail)->Next = (PROCESS *)malloc(sizeof(PROCESS));
+
+	shead->Next = *head;
+	stail = (*tail)->Next;
+	stail->Next = NULL;
+
+	/* bubble sort */
+	for (i=0; i<processNum; i++)
+	{
+		FRONT = shead;
+		MID = FRONT->Next;
+		END = MID->Next;
+
+		while(END!=stail) {
+
+			if(MID->CPUburstTime > END->CPUburstTime) {
+				FRONT->Next = END;
+				MID->Next = END->Next;
+				END->Next = MID;
+			}
+			FRONT = FRONT->Next;
+			MID = FRONT->Next;
+			END = MID->Next;
+		}
+	}
+
+	/* set head and tail */
+	*head = shead->Next;
+	MID->Next = NULL; /* to cut off stail */
+	*tail = FRONT->Next;
+
+	free(shead);
+	free(stail);
+
+}
+
+void SortByPriority(PROCESS **head, PROCESS **tail) {
+
+	PROCESS *FRONT, *MID, *END; /* for swap */
+	PROCESS *shead, *stail, *temp; 
+	int i;
+
+	/* shead -> head -> ... -> tail -> stail */
+	shead = (PROCESS *)malloc(sizeof(PROCESS));
+	stail = (PROCESS *)malloc(sizeof(PROCESS));
+	(*tail)->Next = (PROCESS *)malloc(sizeof(PROCESS));
+
+	shead->Next = *head;
+	stail = (*tail)->Next;
+	stail->Next = NULL;
+
+	/* bubble sort */
+	for (i=0; i<processNum; i++)
+	{
+		FRONT = shead;
+		MID = FRONT->Next;
+		END = MID->Next;
+
+		while(END!=stail) {
+
+			if(MID->Priority > END->Priority) {
+				FRONT->Next = END;
+				MID->Next = END->Next;
+				END->Next = MID;
+			}
+			FRONT = FRONT->Next;
+			MID = FRONT->Next;
+			END = MID->Next;
+		}
+	}
+
+	/* set head and tail */
+	*head = shead->Next;
+	MID->Next = NULL; /* to cut off stail */
+	*tail = FRONT->Next;
+
+	free(shead);
+	free(stail);
+
+}
+
+void SortByRemain(PROCESS **head, PROCESS **tail) {
+
+	PROCESS *FRONT, *MID, *END; /* for swap */
+	PROCESS *shead, *stail, *temp; 
+	int i;
+
+	/* shead -> head -> ... -> tail -> stail */
+	shead = (PROCESS *)malloc(sizeof(PROCESS));
+	stail = (PROCESS *)malloc(sizeof(PROCESS));
+	(*tail)->Next = (PROCESS *)malloc(sizeof(PROCESS));
+
+	shead->Next = *head;
+	stail = (*tail)->Next;
+	stail->Next = NULL;
+
+	/* bubble sort */
+	for (i=0; i<processNum; i++)
+	{
+		FRONT = shead;
+		MID = FRONT->Next;
+		END = MID->Next;
+
+		while(END!=stail) {
+
+			if(MID->RemainTime > END->RemainTime) {
+				FRONT->Next = END;
+				MID->Next = END->Next;
+				END->Next = MID;
+			}
+			FRONT = FRONT->Next;
+			MID = FRONT->Next;
+			END = MID->Next;
+		}
+	}
+
+	/* set head and tail */
+	*head = shead->Next;
+	MID->Next = NULL; /* to cut off stail */
+	*tail = FRONT->Next;
+
+	free(shead);
+	free(stail);
+
+}
+
+void CopyNode(PROCESS *node, PROCESS *new) {
+
+	new->ProcessID = node->ProcessID;
+	new->CPUburstTime = node->CPUburstTime;
+	new->IOburstTime = node->IOburstTime;	
+	new->ArrivalTime = node->ArrivalTime;
+	new->Priority = node->Priority;
+	new->RemainTime = node->RemainTime;
+	new->WaitingTime = node->WaitingTime;
+	new->ExecutingTime = node->ExecutingTime;
+	new->Turnaround = node->Turnaround;
+	new->Next = NULL;	
+
+}
+
+void CopyQueue(PROCESS **queue, PROCESS **head, PROCESS **tail) {
+
+	PROCESS *node;
+	for(; *queue != NULL; *queue = (*queue)->Next) {
+		node = (PROCESS *)malloc(sizeof(PROCESS));
+		CopyNode(*queue, node);
+		Enqueue(node, head, tail);
+	}
+
+}
+
+void FCFS() {
+
+	/* Process Queue */
+	PROCESS *processHead = NULL, *processTail = NULL;
+	PROCESS *processQueue = head, *node, *readyNode;
+
+	int time = 0, endNum = 0;
+	CPUstate = IDLE;
+
+	/* Initialize ReadyQueue */
+	readyHead = NULL;
+	readyTail = NULL;
+
+	/* Copy ProcessQueue */
+	CopyQueue(&processQueue, &processHead, &processTail);
+
+	printf("********************* FCFS Scheduling Report *********************\n\n");
+	printf("                            Gantt Chart                           \n");
+
+	while(endNum != processNum) {
+
+		processQueue = processHead;
+
+		/*  Insert ReadyQueue - Job Scheduling */
+		while(processQueue != NULL && processQueue->ArrivalTime == time) {
+			node = (PROCESS *)malloc(sizeof(PROCESS));
+			CopyNode(processQueue, node);
+			Enqueue(node, &readyHead, &readyTail);
+
+			processQueue = processQueue->Next;
+			processHead = processHead->Next;
+		}
+
+		/* CPU Scheduling */
+		if(CPUstate == SCHEDULE) {
+			CPUstate = IDLE;
+			CopyNode(readyHead, node);
+			Enqueue(node, &endHead, &endTail);
+			readyHead = readyHead->Next;
+		}
+
+		readyNode = readyHead;
+		while(readyNode != NULL) {
+
+			/* cpu state is idle */
+			if(CPUstate == IDLE) {
+				printf("%d",readyNode->ProcessID);
+				CPUstate = readyNode->ProcessID;
+				readyNode->ExecutingTime++;
+				readyNode->RemainTime--;
+				/* end of execute */
+				if(readyNode->RemainTime == 1) {
+					printf("%d",readyNode->ProcessID);
+					readyNode->Turnaround = readyNode->WaitingTime + readyNode->ExecutingTime + 1;
+					endNum++;
+					CPUstate = SCHEDULE;
+				}
+			}
+
+			/* current executing process */
+			else if(CPUstate == readyNode->ProcessID) {
+				printf("-");
+				readyNode->ExecutingTime++;
+				readyNode->RemainTime--;
+				/* end of execute */
+				if(readyNode->RemainTime == 1) {
+					printf("-");
+					readyNode->Turnaround = readyNode->WaitingTime + readyNode->ExecutingTime + 1;
+					endNum++;
+					CPUstate = SCHEDULE;
+				}
+			}
+
+			/* waiting process */
+			else {
+				readyNode->WaitingTime++;
+			}
+			readyNode = readyNode->Next;
+		}
+		time++;
+	}
+	CopyNode(readyHead, node);
+	Enqueue(node, &endHead, &endTail);
 }
 
 int main(void) {
 
+	PROCESS *temp;
+	CreateProcess();
+	temp = head;
+	PrintProcess(&temp);
+	SortByArrival(&head, &tail);
+	temp = head;
+	PrintProcess(&temp);
+	FCFS();
 	return 0;
+
 }
